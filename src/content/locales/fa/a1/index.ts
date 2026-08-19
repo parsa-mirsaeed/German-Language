b3f6b38ae9cc5b76860c3e0f019cf895883678bc
parsa-mirsaeed <parsa.mirsaeed@gmail.com>
2026-08-19 13:57:28 +0330

    Register Persian Units 1 through 4

--- src/content/locales/fa/a1/index.ts
+++ src/content/locales/fa/a1/index.ts
@@ -1,3 +1,12 @@
 import type { LessonLocalization } from "@/i18n/content-localization";
+import { unit01Persian } from "./unit-01-fa";
+import { unit02Persian } from "./unit-02-fa";
+import { unit03Persian } from "./unit-03-fa";
+import { unit04Persian } from "./unit-04-fa";
 
-export const persianA1Localizations: Readonly<Record<string, LessonLocalization>> = {};
+export const persianA1Localizations: Readonly<Record<string, LessonLocalization>> = {
+  [unit01Persian.lessonId]: unit01Persian,
+  [unit02Persian.lessonId]: unit02Persian,
+  [unit03Persian.lessonId]: unit03Persian,
+  [unit04Persian.lessonId]: unit04Persian,
+};
